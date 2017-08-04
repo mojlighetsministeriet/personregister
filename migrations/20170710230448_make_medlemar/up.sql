a8fd2ffb341b5c18ba4a057e0e4171e36ba29dc3
Tomas Bylund <tobychev@live.com>
2017-08-04 21:00:09 +0000

    Fipplade med databasen så att minimala versionen fungera. Fixa en ny migrering sen

--- migrations/20170710230448_make_medlemar/up.sql
+++ migrations/20170710230448_make_medlemar/up.sql
@@ -1,9 +1,9 @@
-CREATE table medlem (
+CREATE table person (
 	uuid varchar(36), primary key(uuid), 
 	namn varchar(512) not null,
-	persNr varchar(16),
+	pers_nr varchar(16),
 	mail varchar(1024),
 	phone varchar(32),
 	street varchar(1024),
-	postNrCity varchar(1024)
+	post_nr_city varchar(1024)
 )
